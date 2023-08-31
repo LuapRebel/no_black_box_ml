a5e1b9441e43972c844ba5ea3af4b406f40fa201
Paul Leber <luaprebel@gmail.com>
2023-08-30 17:57:38 -0700

    Add chart

--- common/utils.js
+++ common/utils.js
@@ -1,35 +1,46 @@
-const utils={};
+const utils = {};
 
-utils.flaggedUsers=
-   [1663882102141,1663900040545,1664485938220];
+utils.flaggedUsers =
+   [1663882102141, 1663900040545, 1664485938220];
 
-utils.formatPercent=(n)=>{
-   return (n*100).toFixed(2)+"%";
+utils.styles = {
+   car: { color: 'gray', text: '🚗' },
+   fish: { color: 'red', text: '🐠' },
+   house: { color: 'yellow', text: '🏠' },
+   tree: { color: 'green', text: '🌳' },
+   bicycle: { color: 'cyan', text: '🚲' },
+   guitar: { color: 'blue', text: '🎸' },
+   pencil: { color: 'magenta', text: '✏️' },
+   clock: { color: 'lightgray', text: '🕒' },
+};
+
+utils.formatPercent = (n) => {
+   return (n * 100).toFixed(2) + "%";
 }
 
-utils.printProgress=(count,max)=>{
+utils.printProgress = (count, max) => {
    process.stdout.clearLine();
    process.stdout.cursorTo(0);
-   const percent=utils.formatPercent(
-      count/max
+   const percent = utils.formatPercent(
+      count / max
    );
-   process.stdout.write(count+"/"+max+
-      " ("+percent+")"
+   process.stdout.write(count + "/" + max +
+      " (" + percent + ")"
    );
 }
 
-utils.groupBy=(objArray,key)=>{
-   const groups={};
-   for(let obj of objArray){
-      const val=obj[key];
-      if(groups[val]==null){
-         groups[val]=[];
+utils.groupBy = (objArray, key) => {
+   const groups = {};
+   for (let obj of objArray) {
+      const val = obj[key];
+      if (groups[val] == null) {
+         groups[val] = [];
       }
       groups[val].push(obj);
    }
    return groups;
 }
 
-if(typeof module!=='undefined'){
-   module.exports=utils;
+if (typeof module !== 'undefined') {
+   module.exports = utils;
 }
