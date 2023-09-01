72dabeea35d1b1a31fd828ca753d3c50f3fb286c
Paul Leber <luaprebel@gmail.com>
2023-09-01 07:03:45 -0700

    Add sketchPad to Data Viewer

--- node/feature_extractor.js
+++ node/feature_extractor.js
@@ -1,5 +1,5 @@
 const constants = require("../common/constants.js");
-const features = require("../common/features.js");
+const featureFunctions = require("../common/featureFunctions.js");
 
 const fs = require('fs');
 
@@ -14,8 +14,8 @@ for (const sample of samples) {
         )
     );
     sample.point = [
-        features.getPathCount(paths),
-        features.getPointCount(paths)
+        featureFunctions.getPathCount(paths),
+        featureFunctions.getPointCount(paths)
     ];
 }
 
