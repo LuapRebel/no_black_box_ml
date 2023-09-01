92b29fe7a0461b98aeb48159e461d0c941a9e1f9
Paul Leber <luaprebel@gmail.com>
2023-09-01 16:50:56 -0700

    normalize training and testing data;

--- node/feature_extractor.js
+++ node/feature_extractor.js
@@ -18,7 +18,6 @@ for (const sample of samples) {
     sample.point = functions.map(f => f(paths));
 }
 
-const minMax = utils.normalizePoints(samples.map(s => s.point));
 
 const featureNames = featureFunctions.inUse.map(f => f.name);
 
@@ -35,6 +34,10 @@ for (let i = 0; i < samples.length; i++) {
         testing.push(samples[i]);
     }
 }
+const minMax = utils.normalizePoints(training.map(s => s.point));
+utils.normalizePoints(
+    testing.map(s => s.point), minMax
+);
 
 fs.writeFileSync(constants.FEATURES,
     JSON.stringify({
